02f3b28d474b72d5680967da0332536233d8a7c0
BertvandenBurg <bertburg@gmail.com>
2025-09-29 23:10:20 +0200

    Eerste twee opdrachten uitgewerkt

---
+++ Les3-PizzaLijst.py
@@ -0,0 +1,23 @@
+# Opdracht 1. In deze oefening ga je een pizza maken. Dat ga je doen door een lijst met toppings te maken
+
+# Stap 1 is zodoende:
+
+# Maak een variabele `toppings` waarin je een lijst met strings als waarde zet. Zorg dat er minimaal 5 toppings/strings
+# op je pizza zitten. Bijvoorbeeld een pizza met salami, kaas, tomaat, ananas en banaan
+
+Toppings = ['gorgonzola', 'champignons', 'tomaat', 'ansjovis', 'ui']
+
+# opdracht 2
+
+# We gaan nu op verschillende manieren onze pizza laten zien.
+# - Laat zien wat jou eerste topping is
+# - Laat zien wat jou derde topping is
+# - Laat zien wat jou laatste topping is
+# - Laat al je toppings zien.
+# Het is natuurlijk de bedoeling dat je deze toppings niet zelf opschrijft, maar dat je ze uit de lijst haalt.
+
+print (f' De eerste topping op mijn pizza is {Toppings[0]}')
+print (f' De derde topping op mijn pizza is {Toppings[2]}')
+print (f' De laatste topping op mijn pizza is {Toppings[-1]}')
+print (f' Mijn pizza bevat de volgende toppings: {Toppings[0:]}')
+
